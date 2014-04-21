9b0773f9bf14b92c19510a3a8b7bff780b4bf33d
omniaura <omniaura5@gmail.com>
2014-04-20 23:37:44 -0700

    recropped image, fade in/out captions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    $('.main').onepage_scroll({});
+    $('.main').onepage_scroll({
+        beforeMove: function(){
+            $('section .caption').fadeOut('slow');
+        },
+        afterMove: function(){
+            $('section.active .caption').delay(500).fadeIn('slow');
+        }
+    });
+    $('section .caption.lead').delay(1500).fadeIn('slow');
 });
